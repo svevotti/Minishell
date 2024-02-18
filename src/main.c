ac29b24141f6b9f6a0445b7b464c7c91c3fa46a0
Sveva Mazzari <smazzari88@gmail.com>
2024-02-18 13:09:00 +0100

    find correct array size and print correctly words

--- src/main.c
+++ src/main.c
@@ -14,13 +14,12 @@ void print_array(char **str)
 int	find_size_array(char *str)
 {
 	int count;
-	int len_str = 0;
+	int	word;
 
 	while (*str == ' ' || *str == '\n' || *str == '\t')
 		str++;
 	count = 0;
-	int word = 1;
-	int l = 0;
+	word = 1;
 	while (*str != '\0')
 	{
 		count++;
@@ -37,71 +36,107 @@ int	find_size_array(char *str)
 			else if (*str == 39)
 			{
 				word = 1;
-				while (*str != 39 || word == 1)
-				{
-					if (*str == 39)
-					{
-						str++;
-						break;
-					}
+				str++;
+				while (*str != 39)
 					str++;
-				}
+				str++;
 			}
 			else if (*str == 34)
 			{
 				word = 1;
-				while (*str != 34 || word == 1)
-				{
-					if (*str == 34)
-					{
-						str++;
-						break;
-					}
+				str++;
+				while (*str != 34)
 					str++;
-				}
+				str++;
 			}
 		}
 		while (*str == ' ' || *str == '\n' || *str == '\t')
 			str++;
-		l++;
 	}
 	return (count);
 }
 
-int find_string_size(char *str, int *l)
+int find_string_size(char *str)
 {
-	int i;
-	char *delimiters;
-	int	j;
-	int	flag;
-	int m;
+	int count;
+	int word;
 
-	
-	flag = 0;
-	i = 0;
-	delimiters = ft_strdup(" \n\t");
-	while (str[i] != '\0')
+	while (*str == ' ' || *str == '\n' || *str == '\t')
+		str++;
+	count = 0;
+	word = 1;
+	while (*str != '\0')
 	{
-		j = 0;
-		while (delimiters[j] != '\0')
+		word = 1;
+		while (word == 1)
 		{
-			if (delimiters[j] == str[i])
+			word = 0;
+			if (*str != 39 && *str != 34 && *str != ' ' && *str != '\n' && *str != '\t' && *str != '\0')
 			{
-				*l = 1;
-				m = i;
-				while(ft_isalpha(str[m]) == 0)
+				word = 1;
+				while (*str != 39 && *str != 34 && *str != ' ' && *str != '\n' && *str != '\t' && *str != '\0')
 				{
-					*l = *l + 1;
-					m++;
+					count++;
+					str++;
 				}
-				*l = *l - 1;
-				return (i);
 			}
-			j++;
+			else if (*str == 39)
+			{
+				word = 1;
+				str++;
+				while (*str != 39)
+				{
+					if (*str == ' ' || *str == '\n' || *str == '\t')
+					{
+						while (*str == ' ' || *str == '\n' || *str == '\t')
+							str++;
+					}
+					else
+					{
+						count++;
+						str++;
+					}
+				}
+				str++;
+			}
+			else if (*str == 34)
+			{
+				word = 1;
+				str++;
+				while (*str != 34)
+				{
+					if (*str == ' ' || *str == '\n' || *str == '\t')
+					{
+						while (*str == ' ' || *str == '\n' || *str == '\t')
+							str++;
+					}
+					else
+					{
+						count++;
+						str++;
+					}
+				}
+				str++;
+			}
 		}
-		i++;
+		if (*str == ' ' || *str == '\n' || *str == '\t')
+			break ;
 	}
-	return (i);
+	return (count);
+}
+
+int check_number_quotes(char *str)
+{
+	int count;
+
+	count = 0;
+	while (*str != '\0')
+	{
+		if (*str == 39 || *str == 34)
+			count++;
+		str++;
+	}
+	return (count);
 }
 
 char	**split_function(char *str)
@@ -115,6 +150,8 @@ char	**split_function(char *str)
 	int		l;
 	int		count;
 	char 	*single_str;
+	int		flag;
+	int		max_quotes;
 
 	i = 0;
 	l = 0;
@@ -124,27 +161,37 @@ char	**split_function(char *str)
 	string_split = (char **)malloc(sizeof(char *) * (size_array + 1));
 	while (i < size_array)
 	{
-		k = 0;
-		count_spaces = 0;
-		size_string = find_string_size(str, &count_spaces);
-		// printf("size array %d\n", size_string);
+		size_string = find_string_size(str);
+		printf("size string %d\n", size_string);
 		single_str = (char *)malloc(sizeof(char) * (size_string + 1));
 		count = 0;
+		k = 0;
+		flag = 0;
 		while (count < size_string)
 		{
-			single_str[k] = str[l];
-			l++;
-			k++;
-			count++;
+			max_quotes = check_number_quotes(str);
+			if (str[l] == 39 || str[l] == 34)
+			{
+				if (flag == max_quotes)
+					break ;
+				l++;
+			}
+			else if (str[l] == ' ' || str[l] == '\n' || str[l] == '\t')
+			{
+				while (str[l] == ' ' || str[l] == '\n' || str[l] == '\t')
+					l++;
+			}
+			else 
+			{
+				single_str[k] = str[l];
+				k++;
+				l++;
+				count++;
+			}
 		}
-		// printf("last char %c\n", str[l]);
 		single_str[k] = '\0';
 		string_split[i] = ft_strdup(single_str);
 		free(single_str);
-		while (str[l] == ' ' || str[l] == '\n' || str[l] == '\t')
-			l++;
-		//printf("single str %s fir size %d\n", single_str, size_string);
-		//printf("size array %d - i %d\n", size_array, i);
 		i++;
 	}
 	string_split[i] = NULL;
@@ -174,7 +221,7 @@ int main(void)
 		// size_input_string = find_size(input_string);
 		// string_to_print = print_env_var(input_string, size_input_string);
 		split_input = split_function(line);
-		// print_array(split_input);
+		print_array(split_input);
 		// printf("%s\n", string_to_print);
 		free(line);
 		//free(string_to_print);
