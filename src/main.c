317f4b00f9994f78755e6233a95bf811c10c0133
Sveva Mazzari <smazzari88@gmail.com>
2024-02-26 12:06:00 +0100

    Use stat to check executable cmd

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "../include/minishell.h"
 #include <dirent.h>
+#include <sys/stat.h>
 
 void print_array(char **str)
 {
@@ -45,7 +46,44 @@ char	*get_path_exec(char *str, char *path_value)
 			i++;
 		}
 	}
-	return ("does not exitst");
+	return (NULL);
+}
+
+char	*create_path_info(char *path, char *cmd)
+{
+	char *full_path;
+	int size;
+	
+	size = ft_strlen(path) + ft_strlen(cmd) + 1;
+	full_path = (char *)malloc(sizeof(char) * (size + 1));
+	ft_strlcpy(full_path, path, ft_strlen(path) + 1);
+	ft_strlcat(full_path, "/", ft_strlen(path) + 1 + 2);
+	ft_strlcat(full_path, cmd, size + 1);
+	return (full_path);
+}
+
+void execute_cmd(char **input, t_env *env)
+{
+	char *cmd_path;
+	char *env_var_value;
+	struct stat stats;
+	char	*cat_path;
+
+	env_var_value = get_env_value(env, "PATH");
+	cmd_path = get_path_exec(input[0], env_var_value);
+	cat_path = create_path_info(cmd_path, input[0]);
+	if (cmd_path != NULL)
+	{
+		if (stat(cmd_path, &stats) == 0)
+		{
+			if (stats.st_mode & X_OK)
+        		printf("execute\n");
+			else
+				printf("not exectuable\n");
+		}
+	}
+	else	
+		printf("error\n");
 }
 
 int main(int argc, char **argv, char **envp)
@@ -55,12 +93,10 @@ int main(int argc, char **argv, char **envp)
 	char		*expanded_input;
 	t_data		data;
   	char     **split_input;
-	char	*path_execve;
-	char 	*path;
+
 	(void)argc;
 	(void)argv;
 	trans_env(&data, envp);
-	path = get_env_value(data.env, "PATH");
 	while (1)
 	{
 		line = readline("Minishell >> ");
@@ -75,9 +111,9 @@ int main(int argc, char **argv, char **envp)
 		size_input_string = find_size(line);
 		expanded_input = expand_input(line, size_input_string, &data);
 		split_input = split_function(expanded_input);
-		print_array(split_input);
-		path_execve = get_path_exec(line, path);
-		printf("%s\n", path_execve);
+		execute_cmd(split_input, data.env);
+		// print_array(split_input);
+		//printf("%s\n", path_execve);
 		free(line);
 	}
 	return (0);
